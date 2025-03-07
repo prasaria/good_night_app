4bb7fb636469b2b6a30351b2eadd2ceeaedf41dc
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 23:27:51 +0700

    remove single forbidden error exception

--- app/exceptions/forbidden_error.rb
+++
@@ -1,8 +0,0 @@
-# app/exceptions/forbidden_error.rb
-module Exceptions
-  class ForbiddenError < StandardError
-    def initialize(message = "You don't have permission to access this resource")
-      super(message)
-    end
-  end
-end
