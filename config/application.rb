65767242af8ebdbf1aa11d83ce210102e0cc9308
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 23:06:13 +0700

    add error handle middleware into rails config application

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+require_relative "../app/exceptions/forbidden_error"
+require_relative "../app/middleware/api_error_handler"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -40,5 +43,7 @@ module GoodNightApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ApiErrorHandler
   end
 end
