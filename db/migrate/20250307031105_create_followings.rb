4070bf7a9b97312f49ee4cc3836583e0b56e6d83
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 12:06:44 +0700

    generate followings table migrations

---
+++ db/migrate/20250307031105_create_followings.rb
@@ -0,0 +1,13 @@
+class CreateFollowings < ActiveRecord::Migration[8.0]
+  def change
+    create_table :followings do |t|
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+
+    # Add a unique index to prevent duplicate followings
+    add_index :followings, [:follower_id, :followed_id], unique: true
+  end
+end
