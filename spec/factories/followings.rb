a19a1c6bc09d220cbf1a92beb39b9fcc94fa45c1
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 13:09:31 +0700

    ensure following factory will create two different instances with unique name

--- spec/factories/followings.rb
+++ spec/factories/followings.rb
@@ -1,7 +1,15 @@
 # spec/factories/followings.rb
+
+# Define a sequence for unique user naming
+FactoryBot.define do
+  sequence :user_sequence
+end
+
+# Define the Following factory
 FactoryBot.define do
   factory :following do
-    association :follower, factory: :user
-    association :followed, factory: :user
+    # Explicitly create unique users with a sequence
+    follower { create(:user, name: "Follower #{generate(:user_sequence)}") }
+    followed { create(:user, name: "Followed #{generate(:user_sequence)}") }
   end
 end
