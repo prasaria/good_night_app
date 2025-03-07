de6a2bdafa0693f117cceedf3ff8ac22b620012c
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 22:36:13 +0700

    placeholder for followings sleep records controller

---
+++ app/controllers/api/v1/followings_sleep_records_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/api/v1/followings_sleep_records_controller.rb
+module Api
+  module V1
+    class FollowingsSleepRecordsController < BaseController
+      # Placeholder for future implementation
+    end
+  end
+end
