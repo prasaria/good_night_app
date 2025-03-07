afb672a235eb196998aa1275ce4892c6a33566f5
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 22:36:26 +0700

    placeholder for sleep records controller

---
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/api/v1/sleep_records_controller.rb
+module Api
+  module V1
+    class SleepRecordsController < BaseController
+      # Placeholder for future implementation
+    end
+  end
+end
