1894b8ee09b357b0fdb9a3f5a7a9efea07caaed9
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 22:35:58 +0700

    placeholder for followings controller

---
+++ app/controllers/api/v1/followings_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/api/v1/followings_controller.rb
+module Api
+  module V1
+    class FollowingsController < BaseController
+      # Placeholder for future implementation
+    end
+  end
+end
