27c667eade262cb8e6b4c55a2c52f114cd0824c0
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-07 12:14:17 +0700

    create factories data for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,26 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
+
+    # Add trait for a user with sleep records
+    trait :with_sleep_records do
+      after(:create) do |user|
+        create_list(:sleep_record, 3, user: user)
+      end
+    end
+
+    # Add trait for a user with followers
+    trait :with_followers do
+      after(:create) do |user|
+        create_list(:following, 2, followed: user)
+      end
+    end
+
+    # Add trait for a user following others
+    trait :following_others do
+      after(:create) do |user|
+        create_list(:following, 2, follower: user)
+      end
+    end
   end
 end
