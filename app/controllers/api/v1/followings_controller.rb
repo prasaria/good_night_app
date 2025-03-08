9e5a0e9feab7271d043dde2badd75405fae4fe8c
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-08 19:35:01 +0700

    implement create methods in the followings controller

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -2,7 +2,20 @@
 module Api
   module V1
     class FollowingsController < BaseController
-      # Placeholder for future implementation
+      # POST /api/v1/followings
+      def create
+        validator = FollowingsValidator.new(params)
+        validator.validate_create_action
+
+        following = Followings::CreateService.new(
+          follower: validator.follower,
+          followed: validator.followed
+        ).call
+
+        render_success({
+          following: FollowingSerializer.new(following, include_users: true).as_json
+        }, :created)
+      end
     end
   end
 end
