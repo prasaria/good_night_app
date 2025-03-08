5c30adf86cf9e7ba74823f26227b66deb8c20153
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-08 17:26:07 +0700

    add index action to sleep records controller

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -2,6 +2,31 @@
 module Api
   module V1
     class SleepRecordsController < BaseController
+      # GET /api/v1/sleep_records
+      def index
+        validator = SleepRecordsValidator.new(params)
+        validator.validate_index_action
+
+        result = SleepRecords::RetrievalService.new(
+          user: validator.user,
+          start_date: validator.start_date,
+          end_date: validator.end_date,
+          from_last_week: params[:from_last_week].present?,
+          completed_only: params[:completed_only].present?,
+          in_progress_only: params[:in_progress_only].present?,
+          sort_by: params[:sort_by],
+          sort_direction: params[:sort_direction],
+          page: params[:page],
+          per_page: params[:per_page],
+          limit: params[:limit]
+        ).call
+
+        render_success({
+          sleep_records: result[:records].map { |record| SleepRecordSerializer.new(record).as_json },
+          pagination: result[:pagination]
+        })
+      end
+
       # POST /api/v1/sleep_records/start
       def start
         validator = SleepRecordsValidator.new(params)
