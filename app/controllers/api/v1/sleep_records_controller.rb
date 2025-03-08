016b1615a5acf239fa7c569a175913a1404807a5
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-08 16:42:10 +0700

    sleep records controller to use error handler middleware

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -5,56 +5,31 @@ module Api
       # POST /api/v1/sleep_records/start
       def start
         validator = SleepRecordsValidator.new(params)
+        validator.validate_start_action
 
-        unless validator.validate_start_action
-          return render_error(validator.error_message, validator.error_status)
-        end
-
-        result = SleepRecords::StartService.new(
+        sleep_record = SleepRecords::StartService.new(
           user: validator.user,
           start_time: validator.start_time
         ).call
 
-        if result.success?
-          render_success({
-            sleep_record: SleepRecordSerializer.new(result.sleep_record).as_json
-          }, :created)
-        else
-          status_code = determine_service_error_status_code(result.errors.first)
-          render_error(result.errors, status_code)
-        end
+        render_success({
+          sleep_record: SleepRecordSerializer.new(sleep_record).as_json
+        }, :created)
       end
 
       # PATCH /api/v1/sleep_records/:id/end
       def end
         validator = SleepRecordsValidator.new(params)
+        validator.validate_end_action(params[:id])
 
-        unless validator.validate_end_action(params[:id])
-          return render_error(validator.error_message, validator.error_status)
-        end
-
-        result = SleepRecords::EndService.new(
+        sleep_record = SleepRecords::EndService.new(
           sleep_record: validator.sleep_record,
           end_time: validator.end_time
         ).call
 
-        if result.success?
-          render_success({ sleep_record: SleepRecordSerializer.new(result.sleep_record).as_json })
-        else
-          status_code = determine_service_error_status_code(result.errors.first)
-          render_error(result.errors, status_code)
-        end
-      end
-
-      private
-
-      def determine_service_error_status_code(error_message)
-        case error_message
-        when /already have an in-progress/i, /cannot be in the future/i, /overlaps/i
-          :unprocessable_entity
-        else
-          :bad_request
-        end
+        render_success({
+          sleep_record: SleepRecordSerializer.new(sleep_record).as_json
+        })
       end
     end
   end
