ecd62e821fd21acbdc182bf2c37497bb6df0ed26
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-09 16:44:28 +0700

    implement cache invalidation in models

--- app/models/following.rb
+++ app/models/following.rb
@@ -8,11 +8,23 @@ class Following < ApplicationRecord
   validates :follower_id, uniqueness: { scope: :followed_id }
   validate :not_follow_self
 
+  # Callbacks
+  after_save :invalidate_caches
+  after_destroy :invalidate_caches
+
   private
 
   def not_follow_self
     if follower_id == followed_id
       errors.add(:followed_id, "can't follow yourself")
     end
+  end
+
+  def invalidate_caches
+    # Invalidate follower's followed users cache
+    Rails.cache.delete_matched("followings/user_#{follower_id}/*") rescue nil
+
+    # Invalidate follower's followed users' sleep records cache
+    Rails.cache.delete_matched("sleep_record_following/user_#{follower_id}/*") rescue nil
   end
 end
--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -19,6 +19,8 @@ class SleepRecord < ApplicationRecord
 
   # Callbacks
   before_save :calculate_duration, if: -> { end_time_changed? && end_time.present? }
+  after_save :invalidate_caches
+  after_destroy :invalidate_caches
 
   # Status checking method
   def completed?
@@ -48,11 +50,23 @@ class SleepRecord < ApplicationRecord
     overlapping_record = user.sleep_records
                               .where.not(id: id) # Exclude current record when updating
                               .where("(start_time <= ? AND (end_time IS NULL OR end_time >= ?))",
-                                     start_time, start_time)
+                                start_time, start_time)
                               .exists?
 
     if overlapping_record
       errors.add(:start_time, "overlaps with another sleep record")
+    end
+  end
+
+  def invalidate_caches
+    # Invalidate user's sleep records cache
+    Rails.cache.delete_matched("sleep_records/user_#{user_id}/*") rescue nil
+
+    # Invalidate following user's cache for sleep records of followed users
+    if user && user.followers.any?
+      user.followers.each do |follower|
+        Rails.cache.delete_matched("sleep_record_following/user_#{follower.id}/*") rescue nil
+      end
     end
   end
 end
